344395f4c37fa0429d02c43a465d68bb1a9db811
Ollie <mrpauffley@gmail.com>
2021-07-26 13:14:01 +0100

    add monsters

--- src/components.rs
+++ src/components.rs
@@ -1,12 +1,16 @@
 pub use crate::prelude::*;
 
-// Render describes the color and symbol needed to draw an entity.
+/// Render describes the color and symbol needed to draw an entity.
 #[derive(Clone, Copy, Debug, PartialEq)]
 pub struct Render {
     pub color: ColorPair,
     pub glyph: FontCharType,
 }
 
-// Player is a tag component to signal an entity is a player.
+/// Player is a tag component to signal an entity is a player.
 #[derive(Clone, Copy, Debug, PartialEq)]
 pub struct Player;
+
+/// Enemy is a tag component to signal an entity is a bad guy.
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub struct Enemy;
