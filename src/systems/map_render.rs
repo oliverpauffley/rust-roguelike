a0d81334e2907ff997135159fc1f516295b58dcb
Ollie <mrpauffley@gmail.com>
2021-08-08 20:57:59 +0100

    create themes

--- src/systems/map_render.rs
+++ src/systems/map_render.rs
@@ -3,7 +3,12 @@ use crate::prelude::*;
 #[system]
 #[read_component(FieldOfView)]
 #[read_component(Player)]
-pub fn map_render(ecs: &SubWorld, #[resource] map: &Map, #[resource] camera: &Camera) {
+pub fn map_render(
+    ecs: &SubWorld,
+    #[resource] map: &Map,
+    #[resource] camera: &Camera,
+    #[resource] theme: &Box<dyn MapTheme>,
+) {
     let mut fov = <&FieldOfView>::query().filter(component::<Player>());
     let mut draw_batch = DrawBatch::new();
     draw_batch.target(0);
@@ -22,10 +27,7 @@ pub fn map_render(ecs: &SubWorld, #[resource] map: &Map, #[resource] camera: &Ca
                 } else {
                     DARK_GRAY
                 };
-                let glyph = match map.tiles[idx] {
-                    TileType::Floor => to_cp437('.'),
-                    TileType::Wall => to_cp437('#'),
-                };
+                let glyph = theme.tile_to_render(map.tiles[idx]);
                 draw_batch.set(pt - offset, ColorPair::new(tint, BLACK), glyph);
             }
         }
